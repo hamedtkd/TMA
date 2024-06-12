302489b1659f0dfff0a482b69775e14f60e40ac0
Hamed Ahmadi <hamed.raadco@gmail.com>
2024-06-12 13:51:38 +0330

    fix:vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import basicSsl from '@vitejs/plugin-basic-ssl';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tsconfigPaths(),],
-    // build: {
-    //   outDir: './docs'
-    // },
-  // base: './'
+  plugins: [react(),tsconfigPaths(), basicSsl()],
+    build: {
+      outDir: './docs'
+    },
+  base: '/'
 });
